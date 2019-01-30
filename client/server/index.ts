58c71ff30dd7d083d2ebc2ba1656873efc122c92
Steven Phan <ptviet@live.com>
2019-01-30 23:43:12 +1100

    fix Heroku deploy

--- client/server/index.ts
+++ client/server/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from "http";
 import { parse } from "url";
-import next from "next";
+const next = require("next");
 import { $log } from "ts-log-debug";
 
 import { routes } from "./routes";
