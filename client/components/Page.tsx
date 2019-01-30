410225fff126480362d17ea6cb44a8e895c422c1
Steven Phan <ptviet@live.com>
2019-01-30 23:15:06 +1100

    add Footer, test Heroku deploy

--- client/components/Page.tsx
+++ client/components/Page.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Head from "next/head";
+import Typography from "@material-ui/core/Typography";
 import Header from "./header/Header";
 
 class Page extends Component {
@@ -11,7 +12,22 @@ class Page extends Component {
         </Head>
         <Header />
         <div>{this.props.children}</div>
-        <div id="footer" />
+        <footer id="footer">
+          <div
+            style={{
+              boxSizing: "border-box",
+              height: 50,
+              textAlign: "center",
+              borderTop: "1px solid #e0e0e0",
+              paddingTop: 30,
+              paddingBottom: 5
+            }}
+          >
+            <Typography variant="caption" gutterBottom>
+              Made with ♥ by STEVEN
+            </Typography>
+          </div>
+        </footer>
       </div>
     );
   }
--- client/config.ts
+++ client/config.ts
@@ -1,3 +1,3 @@
-export const GRAPHQL_URI = "http://localhost:4000";
-export const FRONTEND_URL = "http://localhost:3000";
+export const GRAPHQL_URI = process.env.GRAPHQL_URI || "http://localhost:4000";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 export const pageSize = 18;
