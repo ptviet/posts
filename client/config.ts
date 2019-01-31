6e29c2d253f7934f792187085323a23045b54ca2
Steven Phan <ptviet@live.com>
2019-01-31 14:41:27 +1100

    edit config

--- client/config.ts
+++ client/config.ts
@@ -1,3 +1,9 @@
-export const GRAPHQL_URI = process.env.GRAPHQL_URI || "http://localhost:4000";
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+export const GRAPHQL_URI =
+  process.env.NODE_ENV === "production"
+    ? process.env.GRAPHQL_URI
+    : "http://localhost:4000";
+export const FRONTEND_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.FRONTEND_URL
+    : "http://localhost:3000";
 export const pageSize = 18;
