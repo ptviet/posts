c21f202463838e20bf723581d4105cc89998d6d8
Steven Phan <ptviet@live.com>
2019-02-07 21:36:22 +0700

    fix bs server not found

--- client/src/server.ts
+++ client/src/server.ts
@@ -1,3 +1,111 @@
-import startApp from '../server/server';
+import { createServer } from 'http';
+// import express from 'express';
+// import cors from 'cors';
+import { parse } from 'url';
+import next from 'next';
+import { $log } from 'ts-log-debug';
+import routes from './routes';
+
+// @ts-ignore
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handler = routes.getRequestHandler(app);
+// const handler = app.getRequestHandler();
+
+const startServer = () => {
+  const server = createServer((req: any, res: any) => {
+    const parsedUrl = parse(req.url, true);
+    handler(req, res, parsedUrl);
+  });
+
+  server.listen(port, (err: any) => {
+    if (err) {
+      throw err;
+    }
+    $log.debug(`> Ready on http://localhost:${port}`);
+  });
+
+  // const server = express();
+  // server.use(cors());
+  // server.options('*', cors());
+
+  // server.get('/', (req, res) => {
+  //   const actualPage = '/';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/signin', (req, res) => {
+  //   const actualPage = '/signin';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/signup', (req, res) => {
+  //   const actualPage = '/signup';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/profile', (req, res) => {
+  //   const actualPage = '/profile';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/myposts', (req, res) => {
+  //   const actualPage = '/myposts';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/posts', (req, res) => {
+  //   const actualPage = '/posts';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/post/new', (req, res) => {
+  //   const actualPage = '/newpost';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/post/:_id', (req, res) => {
+  //   const actualPage = '/post';
+  //   const queryParams = { _id: req.params._id };
+  //   app.render(req, res, actualPage, queryParams);
+  // });
+
+  // server.get('/category/new', (req, res) => {
+  //   const actualPage = '/newcategory';
+  //   app.render(req, res, actualPage);
+  // });
+
+  // server.get('/category/:_id', (req, res) => {
+  //   const actualPage = '/category';
+  //   const queryParams = { _id: req.params._id };
+  //   app.render(req, res, actualPage, queryParams);
+  // });
+
+  // server.get('/user/:_id', (req, res) => {
+  //   const actualPage = '/user';
+  //   const queryParams = { _id: req.params._id };
+  //   app.render(req, res, actualPage, queryParams);
+  // });
+
+  // server.all('*', (req, res) => {
+  //   const parsedUrl = parse(req.url, true);
+  //   // if (req.path.startsWith('/graphql')) return next();
+
+  //   return handler(req, res, parsedUrl);
+  //   // return handler(req, res, parsedUrl, next);
+  // });
+
+  // server.listen(port, (err: any) => {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   $log.debug(`> Ready on http://localhost:${port}`);
+  // });
+};
+
+const startApp = () => {
+  app.prepare().then(() => startServer());
+};
 
 startApp();
